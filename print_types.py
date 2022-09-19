39501dd1d74f613beea943fdb27fd3f5017ed2df
Shainaz <102401756+shainy786@users.noreply.github.com>
2022-09-19 00:23:20 -0700

    Create print_types.py

---
+++ print_types.py
@@ -0,0 +1,37 @@
+print()
+print(',')
+print('')
+print(" ")
+print(""" """)
+print(''' ''')
+
+print('iam sahiny')
+print("iam shainy")
+print("""iam shainy""")
+print('''iam shainy''')
+
+print('i\'m shainy')
+print("iam shai\"ny")
+print('''i'm shainy''')
+print("""i'm shainy""")
+
+print(print('hello'))
+print('hello,world')
+print('hello world')
+print('hello','world')
+print('hello',               'world')
+print("hello"+" world")
+print("hello"+' '+"world")
+
+print("""hello
+world""")
+print('''hello
+world''')
+print("""hello\nworld""")
+print('''hello\nworld''')
+
+print("python "*5)
+print(5*"python")
+print(type("python"))
+print(type(2))
+print(type(7.8))
